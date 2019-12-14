211fef15e5ff5c73c60112d464b889ebea2ffff3
mmmaxxx <max.webhk@gmail.com>
2019-12-14 08:18:54 +0800

    Add engine to package json

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const port = 4100;
 
-app.post('webhook', (req, res) => {
-    res.send('Got a POST request')
+app.get('/', (req, res) => {
+    res.send('Received a GET request')
+});
+
+app.post('/webhook', (req, res) => {
+    console.log('Got a POST REQUEST');
+    res.send('Got a POST request ')
 });
 
 app.listen(port, () => console.log('I am listening on port ' + port));
