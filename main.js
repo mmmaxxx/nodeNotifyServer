3fb14f1a7f3d8ef93346aeeab914759160769982
mmmaxxx <max.webhk@gmail.com>
2019-12-14 09:13:58 +0800

    Testing implementation of socket io

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 4100;
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
     res.send('Received a GET request')
 });
 
+io.on('connection', (socket) => {
+    console.log('A user connected');
+});
+
 app.post('/webhook', (req, res) => {
+    io.emit('notif', 'test');
     console.log('Got a POST REQUEST');
     res.send('Got a POST request ')
 });
 
-app.listen(process.env.PORT || port, () => console.log('I am listening on port ' + port));
+http.listen(process.env.PORT || 4100, () => console.log('I am listening on port ' + process.env.PORT));
